a45df0175aa1fce16dbc7c4dd16849249abf1b6a
yingnengd <80525738+yingnengd@users.noreply.github.com>
2025-12-18 18:11:57 +0800

    Update __init__.py

--- recipes/inference/custom_music_generation/full_song/model/__init__.py
+++ recipes/inference/custom_music_generation/full_song/model/__init__.py
@@ -1,7 +1,1 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
 
-from ...generation import Llama
-# from .model import ModelArgs, Transformer
-# from .tokenizer import Dialog, Tokenizer
-#__init__.py
