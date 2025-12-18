68f60650ab535655fc0204b2942c1a21dd49a259
yingnengd <80525738+yingnengd@users.noreply.github.com>
2025-12-18 15:33:15 +0800

    Create song_structure.py

---
+++ recipes/inference/custom_music_generation/full_song/engine/song_structure.py
@@ -0,0 +1,12 @@
+SECTION_PLAN = [
+    ("intro", 8),
+    ("verse", 16),
+    ("pre", 8),
+    ("chorus", 8),
+    ("verse", 16),
+    ("pre", 8),
+    ("chorus", 8),
+    ("bridge", 8),
+    ("final", 12),
+    ("outro", 4)
+]
