d96d81fb23845ab3dfbd78912dea85e30ca1a9cc
yingnengd <80525738+yingnengd@users.noreply.github.com>
2025-12-18 22:24:34 +0800

    Create music_llama_model.py

---
+++ recipes/inference/custom_music_generation/full_song/model/music_llama_model.py
@@ -0,0 +1,19 @@
+from model.music_llama import MusicLLaMA
+from model.base_model import BaseMusicModel
+
+class MusicLlamaModel(BaseMusicModel):
+    def __init__(self, ckpt_path, device="mps"):
+        self.model = MusicLLaMA(
+            ckpt_path=ckpt_path,
+            device=device
+        )
+
+    def generate(self, prompt_midi, bars, constraints):
+        # 参数名对齐 + 转发
+        return self.model.generate(
+            prompt_midi=prompt_midi,
+            n_bars=bars,
+            constraints=constraints,
+            temperature=0.9,
+            top_p=0.95
+        )
