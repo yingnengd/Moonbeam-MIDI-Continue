0b06ec6ab57b5f08af888f7befbcd224ce85c20f
yingnengd <80525738+yingnengd@users.noreply.github.com>
2025-12-18 19:35:53 +0800

    Update scale_engine.py

--- recipes/inference/custom_music_generation/full_song/engine/scale_engine.py
+++ recipes/inference/custom_music_generation/full_song/engine/scale_engine.py
@@ -1,3 +1,4 @@
+'''
 def get_allowed_scale(section, mode):
     if section == "verse":
         return [0,2,4,7,9] if mode == "major" else [0,3,5,7,10]
@@ -7,3 +8,16 @@ def get_allowed_scale(section, mode):
             if mode == "major"
             else [0,2,3,5,7,8,10]
         )
+'''
+
+SCALES = {
+    "major":        [0, 2, 4, 5, 7, 9, 11],
+    "minor":        [0, 2, 3, 5, 7, 8, 10],
+    "pentatonic":   [0, 2, 4, 7, 9],
+    "minor_penta":  [0, 3, 5, 7, 10],
+    "dorian":       [0, 2, 3, 5, 7, 9, 10],
+    "mixolydian":   [0, 2, 4, 5, 7, 9, 10],
+}
+
+def get_allowed_scale(tonic, scale_name="major"):
+    return [(tonic + i) % 12 for i in SCALES[scale_name]]
